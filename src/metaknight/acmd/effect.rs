bb25484a05730e9d12d2a19686404a850a8c9d5a
LKE-Studios <102039918+LKE-Studios@users.noreply.github.com>
2022-12-17 09:54:17 +1000

    Added some acmd stuff and frame data for MK

---
+++ src/metaknight/acmd/effect.rs
@@ -0,0 +1,146 @@
+use smash::app::sv_animcmd::*;
+use smash::phx::Hash40;
+use smash::lib::lua_const::*;
+use smash::app::lua_bind::*;
+use smashline::*;
+use smash_script::*;
+use smash::lua2cpp::L2CAgentBase;
+
+#[acmd_script(//GlideStart
+    agent = "metaknight", 
+    script = "effect_glidestart", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_glide1gfx(fighter: &mut L2CAgentBase) {
+    frame(fighter.lua_state_agent, 2.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_sword"), Hash40::new("haver"), 0.0, 0, 0, 0, 0, 0, 1, true);
+        macros::EFFECT(fighter, Hash40::new("sys_smash_flash"), Hash40::new("top"), -5.3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("sys_aura_light"), Hash40::new("top"), 0.0, 0, 0, 0, 0, 0, 6.4, true);
+        macros::LAST_EFFECT_SET_COLOR(fighter, /*R*/ 0.68, /*G*/ 0.87, /*B*/ 2.0);
+    }
+}
+
+#[acmd_script(//GlideWing
+    agent = "metaknight", 
+    script = "effect_glidewing", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_glide2gfx(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("metaknight_sword"), false, false);
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("sys_aura_light"), false, false);
+    }
+}
+
+#[acmd_script(//GlideAttack
+    agent = "metaknight", 
+    script = "effect_glideattack", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_glideattackgfx(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_sword"), Hash40::new("haver"), 0.0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(fighter.lua_state_agent, 4.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_air_lw"), Hash40::new("top"), 0.0, 8.5, 0, -41.3, 28.5, -6.0, 1.9, true);
+    }
+    frame(fighter.lua_state_agent, 9.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("metaknight_sword"), false, false);
+    }
+}
+
+#[acmd_script(//GlideLanding
+    agent = "metaknight", 
+    script = "effect_glidelanding", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_glidelandinggfx(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        macros::LANDING_EFFECT(fighter, Hash40::new("sys_landing_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1.0, 0, 0, 0, 0, 0, 0, false);
+    }
+}
+
+#[acmd_script(//AttackAirHi
+    agent = "metaknight", 
+    script = "effect_attackairhi", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_uairgfx(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_sword"), Hash40::new("haver"), 0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(fighter.lua_state_agent, 2.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_air_hi"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1.05, true);
+        EffectModule::set_disable_render_offset_last(fighter.module_accessor);
+    }
+    frame(fighter.lua_state_agent, 9.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("metaknight_sword"), false, false);
+    }
+}
+
+#[acmd_script(//SpecialHi
+    agent = "metaknight", 
+    script = "effect_specialhi", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_upbgfx(fighter: &mut L2CAgentBase) {
+    frame(fighter.lua_state_agent, 1.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_sword"), Hash40::new("haver"), 0.0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(fighter.lua_state_agent, 5.0);
+    if macros::is_excute(fighter) {
+        macros::LANDING_EFFECT(fighter, Hash40::new("sys_v_smoke_b"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+    }
+    frame(fighter.lua_state_agent, 8.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_shuttleloop1"), Hash40::new("top"), 0.0, 0, 0, 0, 0, 0, 1.2, true);
+        EffectModule::set_disable_render_offset_last(fighter.module_accessor);
+    }
+    frame(fighter.lua_state_agent, 9.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT(fighter, Hash40::new("sys_smash_flash_s"), Hash40::new("top"), 0, 20, 15, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+    }
+    frame(fighter.lua_state_agent, 30.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("metaknight_sword"), false, false);
+    }
+}
+
+#[acmd_script(//SpecialHiLoop
+    agent = "metaknight", 
+    script = "effect_specialhiloop", 
+    category = ACMD_EFFECT, 
+    low_priority )]
+unsafe fn metaknight_upbloopgfx(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_sword"), Hash40::new("haver"), 0.0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(fighter.lua_state_agent, 2.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_FOLLOW(fighter, Hash40::new("metaknight_shuttleloop1"), Hash40::new("top"), 0, -5, 2.5, 4, 0, 0, 1, true);
+        EffectModule::set_disable_render_offset_last(fighter.module_accessor);
+        macros::EFFECT(fighter, Hash40::new("sys_smash_flash_s"), Hash40::new("top"), 0, 20, 15, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+    }
+    frame(fighter.lua_state_agent, 31.0);
+    if macros::is_excute(fighter) {
+        macros::EFFECT_OFF_KIND(fighter, Hash40::new("metaknight_sword"), false, false);
+    }
+}
+
+pub fn install() {
+    smashline::install_acmd_scripts!(
+        metaknight_glide1gfx,
+        metaknight_glide2gfx,
+        metaknight_glideattackgfx,
+        metaknight_glidelandinggfx,
+        metaknight_uairgfx,
+        metaknight_upbgfx,
+        metaknight_upbloopgfx,
+    );
+}
