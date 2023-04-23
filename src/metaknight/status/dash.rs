4e336a9b688ca5558c91017c3587161ae70cbe76
WuBoytH <alexwuief@gmail.com>
2023-04-23 09:08:26 -0400

    Forgot to make turn dash trip

--- src/metaknight/status/dash.rs
+++ src/metaknight/status/dash.rs
@@ -2,8 +2,19 @@ use crate::imports::status_imports::*;
 
 #[status_script( agent = "metaknight", status = FIGHTER_STATUS_KIND_DASH, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE )]
 unsafe fn metaknight_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
-    let rand = sv_math::rand(hash40("fighter"), 100);
-    if rand == 0 {
+    let rand = sv_math::rand(hash40("fighter"), 1000);
+    if rand <= 99 {
+        StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_SLIP);
+        return 1.into();
+    }
+    original!(fighter)
+}
+
+#[status_script( agent = "metaknight", status = FIGHTER_STATUS_KIND_TURN_DASH, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE )]
+unsafe fn metaknight_turn_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    let rand = sv_math::rand(hash40("fighter"), 1000);
+    if rand <= 124 {
+        PostureModule::reverse_lr(fighter.module_accessor);
         StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_SLIP);
         return 1.into();
     }
@@ -12,6 +23,7 @@ unsafe fn metaknight_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
 
 pub fn install() {
     install_status_scripts!(
-        metaknight_dash_pre
+        metaknight_dash_pre,
+        metaknight_turn_dash_pre
     );
 }
