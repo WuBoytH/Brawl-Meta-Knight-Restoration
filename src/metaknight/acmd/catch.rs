4be8362a7d6082ed6364a007f31dadced452e684
LKE-Studios <102039918+LKE-Studios@users.noreply.github.com>
2023-04-25 07:09:15 +1000

    Added correct pummel frame data

--- src/metaknight/acmd/catch.rs
+++ src/metaknight/acmd/catch.rs
@@ -63,14 +63,30 @@ unsafe fn metaknight_catchturn(fighter: &mut L2CAgentBase) {
 
 #[acmd_script( agent = "metaknight", script = "game_catchattack", category = ACMD_GAME )]
 unsafe fn metaknight_catchattack(fighter: &mut L2CAgentBase) {
-    frame(fighter.lua_state_agent, 1.0);
+    wait(fighter.lua_state_agent, 3.0);
     if macros::is_excute(fighter) {
         macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 3.0, 361, 100, 30, 0, 5.5, 0.0, 11.0, 9.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_BODY);
         AttackModule::set_catch_only_all(fighter.module_accessor, true, false);
     }
-    wait(fighter.lua_state_agent, 2.0);
+    wait(fighter.lua_state_agent, 1.0);
     if macros::is_excute(fighter) {
         AttackModule::clear_all(fighter.module_accessor);
+    }
+}
+
+#[acmd_script( agent = "metaknight", script = "expression_catchattack", category = ACMD_EXPRESSION, low_priority )]
+unsafe fn metaknight_catchattack_expression(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        slope!(fighter, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
+        VisibilityModule::set_status_default_int64(fighter.module_accessor, Hash40::new("mantle"), Hash40::new("mantle_wing"));
+    }
+    wait(fighter.lua_state_agent, 3.0);
+    if macros::is_excute(fighter) {
+        macros::RUMBLE_HIT(fighter, Hash40::new("rbkind_attacks"), 0);
+    }
+    frame(fighter.lua_state_agent, 21.0);
+    if macros::is_excute(fighter) {
+        VisibilityModule::set_status_default_int64(fighter.module_accessor, Hash40::new("mantle"), Hash40::new("mantle_normal"));
     }
 }
 
@@ -79,6 +95,7 @@ pub fn install() {
         metaknight_catch,
         metaknight_catchdash,
         metaknight_catchturn,
-        metaknight_catchattack
+        metaknight_catchattack,
+        metaknight_catchattack_expression
     );
 }
