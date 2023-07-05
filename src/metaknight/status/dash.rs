50b0a06d633685da6f355f6a536f450d5e44f64f
LKE-Studios <102039918+LKE-Studios@users.noreply.github.com>
2023-07-05 12:25:44 +1000

    10000

--- src/metaknight/status/dash.rs
+++ src/metaknight/status/dash.rs
@@ -2,7 +2,7 @@ use crate::imports::status_imports::*;
 
 #[status_script( agent = "metaknight", status = FIGHTER_STATUS_KIND_DASH, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE )]
 unsafe fn metaknight_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
-    let rand = sv_math::rand(hash40("fighter"), 1000);
+    let rand = sv_math::rand(hash40("fighter"), 10000);
     if rand <= 99 {
         StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_SLIP);
         return 1.into();
