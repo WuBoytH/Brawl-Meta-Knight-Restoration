4a3293ba2bd981e71e98383cdc194143c5eb80b4
WuBoytH <alexwuief@gmail.com>
2023-07-04 22:16:48 -0400

    Sigh.

--- src/metaknight/status/dash.rs
+++ src/metaknight/status/dash.rs
@@ -12,7 +12,7 @@ unsafe fn metaknight_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
 
 #[status_script( agent = "metaknight", status = FIGHTER_STATUS_KIND_TURN_DASH, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE )]
 unsafe fn metaknight_turn_dash_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
-    let rand = sv_math::rand(hash40("fighter"), 1000);
+    let rand = sv_math::rand(hash40("fighter"), 10000);
     if rand <= 124 {
         PostureModule::reverse_lr(fighter.module_accessor);
         StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_SLIP);
